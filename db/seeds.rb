59e707484dbfcd29d8e3dcbd52699717d3091748
benjamin bavuz <benjamin.bavuz@gmail.com>
2017-02-15 18:54:58 +0100

    yelp rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  restaurant = Restaurant.create(name: Faker::Name.last_name, address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
